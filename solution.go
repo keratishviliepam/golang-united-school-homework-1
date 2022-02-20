17b831eaf7d41817a72b423bff00404c8e278cce
gadmin <galphaa>
2022-02-20 14:10:02 +0400

    adding emoji

--- solution.go
+++ solution.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/kyokomi/emoji/v2"
+        "github.com/kyokomi/emoji/v2"
 )
 
 func main() {
-    worldMessage := emoji.Sprint("Hello :world_map:!")
-    fmt.Println(worldMessage)
+        GetMessage()
+}
+
+func GetMessage() string {
+        worldMessage := emoji.Sprint("Hello :world_map:!")
+
+        return worldMessage
 }
